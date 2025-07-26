6b7cc21e9d284d2dd458f6d94c342f4228ab2d80
Edgar-klewert <EDGARKLEWERT66@GMAIL.COM>
2025-07-26 17:28:56 -0300

    Remove duplicated folders outside app

--- utils/menu.py
+++
@@ -1,8 +0,0 @@
-def exibir_menu():
-    print("\n=== Gerenciador de Livros ===")
-    print("1. Cadastrar Livro")
-    print("2. Listar Livros")
-    print("3. Buscar Livro por ID")
-    print("4. Atualizar Livro")
-    print("5. Deletar Livro")
-    print("0. Sair")
