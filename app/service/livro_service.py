a89dbcde8eb245b0557b503e628e472826c64e81
Edgar-klewert <EDGARKLEWERT66@GMAIL.COM>
2025-09-03 15:19:59 -0300

    feat: add title search and sortable listing in book service

--- app/service/livro_service.py
+++ app/service/livro_service.py
@@ -5,8 +5,8 @@ def criar_livro(dados):
     livro = Livro(**dados)
     repo.salvar_livro(livro)
 
-def listar_livros():
-    return repo.listar_livros()
+def listar_livros(coluna):
+    return repo.listar_livros(coluna)
 
 def buscar_livro(id):
     return repo.buscar_por_id(id)
@@ -17,3 +17,6 @@ def remover_livro(id):
 def editar_livro(id, dados):
     livro = Livro(**dados)
     repo.atualizar_livro(id, livro)
+
+def buscar_livro_por_titulo(titulo):
+    return repo.buscar_por_titulo(titulo)
